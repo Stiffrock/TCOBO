c3c18adb4f717734a22bfea41065d7bb0f20ddc3
Stiffrock <christofer.malmberg@gmail.com>
2015-05-14 16:26:07 +0200

    menuEndSeq
    Sexy menu end sequence

--- TCOBO/TCOBO/MenuManager.cs
+++ TCOBO/TCOBO/MenuManager.cs
@@ -14,24 +14,25 @@ namespace TCOBO
 
     class MenuManager
     {
-        public bool GameOn = false, drawTime = false;
+        public bool GameOn = false, endSeq = false;
         private SpriteFont menuTitle;
         private SpriteFont menuText;
         private KeyMouseReader krm;
         private Vector2 textpos;
-        private string tcobo;
+        private string tcobo, title;
         private List<string> stringList = new List<string>();
         private Random rnd = new Random();
-        private float deltaTime = 200f;
-        private float time;
+        private float deltaTime = 10f, deltaTime2 = 20f, time, time2, textspeed, textCount = 40,
+            endseqTimer = 200;
+        private List<float> xposList = new List<float>();
+        private List<float> yposList = new List<float>();
+        private List<char> charlist = new List<char>();
+        private List<Color> colorList = new List<Color>();
+        private Color flowColor;
+        private Vector2 coverPos = new Vector2(300,300);
+        private Rectangle coverRec = new Rectangle(0,0,670,50);
         private int textmoveY = 0;
         private int textmoveX = 0;
-        private int textmoveY2 = 0;
-        private int textmoveX2 = 0;
-        private int textmoveY3 = 0;
-        private int textmoveX3 = 0;
-        private int textmoveY4 = 0;
-        private int textmoveX4 = 0;
         
 
 
@@ -40,61 +41,114 @@ namespace TCOBO
             menuTitle = game1.Content.Load<SpriteFont>("MenuTitleFont");
             menuText = game1.Content.Load<SpriteFont>("MenuFont");
             krm = new KeyMouseReader();
+            flowColor = Color.Blue;
             tcobo = "T\nh\ne\nr\ne\n \nc\na\nn\n \no\nn\nl\ny\n \nb\ne\n \no\nn\ne\n";
+
+        }
+
+
+
+        public void endseqText(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(menuTitle, "THERE CAN ONLY BE ONE", new Vector2(300, 300), Color.Blue);
+            spriteBatch.Draw(TextureManager.bricktile1, coverPos, coverRec, Color.Black);
         }
 
         public void bgText(SpriteBatch spriteBatch)
         {
-              for (int i = 0; i < 100; i++)
-              {
+         /*   for (int i = 0; i < 100; i++)     Gammal textSpam utan timer bara fps cap
+            {
                 int textmove = rnd.Next(0, 10000);
-                 spriteBatch.DrawString(menuText, tcobo, new Vector2(textpos.X + i * 15, textpos.Y + textmove), Color.Blue);
-              }
+                spriteBatch.DrawString(menuText, tcobo, new Vector2(textpos.X + i * 15, textpos.Y + textmove), Color.Blue);
+            }*/
 
-         
-            if (time >= deltaTime)
-            {              
-                textmoveY = rnd.Next(0, 1280);
-                textmoveX = rnd.Next(0, 1280);
-                textmoveY2 = rnd.Next(0, 1280);
-                textmoveX2 = rnd.Next(0, 1280);
-                textmoveY3 = rnd.Next(0, 1280);
-                textmoveX3 = rnd.Next(0, 1280);
-                textmoveY4 = rnd.Next(0, 1280);
-                textmoveX4 = rnd.Next(0, 1280);
-                time = 0;
+            if (KeyMouseReader.LeftClick())
+            {
+                endSeq = true;
             }
-                spriteBatch.DrawString(menuText, tcobo, new Vector2(textpos.X + textmoveX, textpos.Y + textmoveY), Color.Blue);
-                spriteBatch.DrawString(menuText, tcobo, new Vector2(textpos.X + textmoveX2, textpos.Y + textmoveY2), Color.Blue);
-                spriteBatch.DrawString(menuText, tcobo, new Vector2(textpos.X + textmoveX3, textpos.Y + textmoveY3), Color.Blue);
-                spriteBatch.DrawString(menuText, tcobo, new Vector2(textpos.X + textmoveX4, textpos.Y + textmoveY4), Color.Blue); 
-            
-            
-             
+
+            if (time >= deltaTime)
+            {               
+                for (int i = 0; i < textCount; i++)
+                {
+                    textmoveY = rnd.Next(-200, 1000);
+                    textmoveX = rnd.Next(0, 1280);
+                    xposList.Add(textmoveX);
+                    yposList.Add(textmoveY);                 
+                }
+              
+                if (xposList.Count != 0 && yposList.Count != 0)
+                {
+                    
+                    for (int i = 0; i < textCount; i++)
+                    {          
+                        spriteBatch.DrawString(menuText, tcobo, new Vector2(textpos.X + xposList[i], textpos.Y + yposList[i]), flowColor);
+                    }
+                }
+
+                if (time >= deltaTime2)
+                {
+                    for (int i = 0; i < textCount; i++)
+                    {
+                        xposList.Remove(xposList[i]);
+                        yposList.Remove(yposList[i]);
+                    }  
+                    time = 0;
+                    if (endSeq)
+                    {                    
+                        textCount -= 0.5f;
+                    }                   
+                }
+            }                                         
+        }
+
+        private void textposUpdate()
+        {
+            if (yposList.Count != 0)
+            {
+                float textspeed;
+                for (int i = 0; i < 40; i++)
+                {
+                    textspeed = rnd.Next(25, 50);
+                    yposList[i] += textspeed;
+                }
+            }
+
         }
 
         public void Update(GameTime gameTime)
         {
+            if (textCount <= 0 )
+            {
+                GameOn = true;
+            }
+            if (endSeq)
+            {
+                coverRec.Width -= 10;
+                coverPos.X += 10;
+            }
   
             time += gameTime.ElapsedGameTime.Milliseconds;
-
+            time2 += gameTime.ElapsedGameTime.Milliseconds;
             krm.Update();
-            Console.WriteLine(stringList.Count);
-
-            if (KeyMouseReader.LeftClick())
-            {
-                   GameOn = true;
-            }
-
+            textposUpdate();
+           
+             
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
             bgText(spriteBatch);
-            spriteBatch.DrawString(menuTitle, "TCOBO ", new Vector2(540, 260), Color.Blue);
-            spriteBatch.DrawString(menuText, "Click to start", new Vector2(540, 360), Color.Blue);
-
+            if (endSeq)
+            {
+                endseqText(spriteBatch);
+            }
+            if (!endSeq)
+            {
+                spriteBatch.DrawString(menuTitle, "TCOBO ", new Vector2(540, 260), Color.Blue);
+                spriteBatch.DrawString(menuText, "Click to start", new Vector2(540, 360), Color.Blue);               
+            }
             spriteBatch.End();
         }
 
