e3fd8dce2ba1a8ab0a7cc2211222700847c75fd6
Stiffrock <christofer.malmberg@gmail.com>
2015-05-23 16:16:10 +0200

    Merge remote-tracking branch 'origin/Stoffe' into Stoffe
    Conflicts:
    	TCOBO.v12.suo
    	TCOBO/TCOBO/ParticleEngine.cs
    	TCOBO/TCOBO/TCOBO.csproj.Debug.cachefile
    	TCOBO/TCOBO/bin/x86/Debug/Content/Death sound.wma
    	TCOBO/TCOBO/bin/x86/Debug/Content/Ljudeffekt1.wma
    	TCOBO/TCOBO/bin/x86/Debug/Content/TCOBO musik.wma
    	TCOBO/TCOBO/bin/x86/Debug/TCOBO.exe
    	TCOBO/TCOBO/bin/x86/Debug/TCOBO.pdb
    
    	TCOBO/TCOBO/obj/x86/Debug/ContentPipeline-{F57F5713-2EE6-4A39-821D-E7329A4EA41E}.xml
    	TCOBO/TCOBO/obj/x86/Debug/TCOBO.csproj.FileListAbsolute.txt
    	TCOBO/TCOBO/obj/x86/Debug/TCOBO.exe
    	TCOBO/TCOBO/obj/x86/Debug/TCOBO.pdb
    
    	TCOBO/TCOBO/obj/x86/Debug/cachefile-{F57F5713-2EE6-4A39-821D-E7329A4EA41E}-targetpath.txt
    
    	TCOBO/TCOBOContent/obj/x86/Debug/TCOBOContent.contentproj.FileListAbsolute.txt

--- TCOBO/TCOBO/ParticleEngine.cs
+++ TCOBO/TCOBO/ParticleEngine.cs
@@ -42,12 +42,7 @@ namespace TCOBO
                 0.1f + (float)random.NextDouble(),
                 0,
                  0);
-<<<<<<< HEAD
-            float size = (float)random.NextDouble();
-=======
-            float size = (float)random.NextDouble()/2;
->>>>>>> origin/Stoffe
-
+            float size = (float)random.NextDouble() +0.1f;
             int ttl = 100 + random.Next((int)1500);
 
             return new Particle(texture, position, velocity, angle, angularVelocity, color, size, ttl);
